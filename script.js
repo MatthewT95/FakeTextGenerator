eacb23d8562e4b169c887c694ba8c45626c8cc43
MatthewT95 <matt.taormina@outlook.com>
2024-07-14 22:34:41 -0400

    Can generate text by paragraph length or sentence count. Also can produce raw html now.

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
+let unitCount = 3;
+let unitType = "p";
+let plainText = false;
+
 function refreshContent() {
   let outputElement = document.getElementById("content");
 
   outputElement.innerHTML = "";
 
-  for (let i = 0; i < 12; i++) {
-    let paragraphElement = document.createElement("p");
-    paragraphElement.innerHTML = randomFakeParagraph(7, 18);
-    outputElement.append(paragraphElement);
+  if (plainText) {
+    if (unitType == "p") {
+      for (let i = 0; i < unitCount; i++) {
+        outputElement.innerHTML += "<p>" + randomFakeParagraph() + "</p>";
+      }
+    } else if (unitType == "s") {
+      let text = "";
+      for (let i = 0; i < unitCount; i++) {
+        text += randomFakeSentence();
+      }
+      outputElement.innerHTML = "<p>" + text + "</p>";
+    }
+  } else {
+    if (unitType == "p") {
+      for (let i = 0; i < unitCount; i++) {
+        outputElement.innerHTML +=
+          "&lt;p&gt;<br>" + randomFakeParagraph() + "<br>&lt;/p&gt;<br>";
+      }
+    } else if (unitType == "s") {
+      let text = "";
+      for (let i = 0; i < unitCount; i++) {
+        text += randomFakeSentence();
+      }
+      outputElement.innerHTML = "&lt;p&gt;<br>" + text + "<br>&lt;/p&gt;";
+    }
+  }
+
+  if (!plainText) {
+    outputElement.innerHTML = "<code>" + outputElement.innerHTML + "</code>";
   }
 }
 
 let btnGenerate = document.getElementById("generate-text");
 btnGenerate.addEventListener("click", refreshContent);
+fakeTextGeneratorSettings.paragraphMinLength = 10;
+fakeTextGeneratorSettings.paragraphMaxLength = 15;
+fakeTextGeneratorSettings.sentenceMinLength = 8;
+fakeTextGeneratorSettings.sentenceMaxLength = 15;
 refreshContent();
