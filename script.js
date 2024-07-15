393fe3d892989d4396d828d9821f5bcf8749d2db
MatthewT95 <matt.taormina@outlook.com>
2024-07-15 14:32:07 -0400

    Updated interface to be able regenerate, append and prepend content.

--- script.js
+++ script.js
@@ -3,13 +3,53 @@ let unitType = "s";
 let webPreviewMode = true;
 let outputElement = document.getElementById("content");
 
-function refreshContent() {
+function regenerateAllContent() {
+  let webPreviewModeHistory = webPreviewMode;
+  if (!webPreviewMode) {
+    toggleFormat();
+  }
+
   if (unitType == "p") {
     paragraphFill(outputElement, "u", unitCount, [], ["p"]);
   } else if (unitType == "s") {
     sentenceFill(outputElement, "u", unitCount, ["p"], []);
   }
-  webPreviewMode = true;
+
+  if (!webPreviewModeHistory) {
+    toggleFormat();
+  }
+}
+
+function appendContent() {
+  let webPreviewModeHistory = webPreviewMode;
+  if (!webPreviewMode) {
+    toggleFormat();
+  }
+  if (unitType == "p") {
+    paragraphFill(outputElement, "a", unitCount, [], ["p"]);
+  } else if (unitType == "s") {
+    sentenceFill(outputElement, "a", unitCount, ["p"], []);
+  }
+
+  if (!webPreviewModeHistory) {
+    toggleFormat();
+  }
+}
+
+function prependContent() {
+  let webPreviewModeHistory = webPreviewMode;
+  if (!webPreviewMode) {
+    toggleFormat();
+  }
+  if (unitType == "p") {
+    paragraphFill(outputElement, "p", unitCount, [], ["p"]);
+  } else if (unitType == "s") {
+    sentenceFill(outputElement, "p", unitCount, ["p"], []);
+  }
+
+  if (!webPreviewModeHistory) {
+    toggleFormat();
+  }
 }
 
 function toggleFormat() {
@@ -33,10 +73,16 @@ function toggleFormat() {
   }
 }
 
-let btnGenerate = document.getElementById("generate-text");
-btnGenerate.addEventListener("click", refreshContent);
+let btnRegenerate = document.getElementById("btnRegenerate");
+let btnAppend = document.getElementById("btnAppend");
+let btnPrepend = document.getElementById("btnPrepend");
+
+btnRegenerate.addEventListener("click", regenerateAllContent);
+btnAppend.addEventListener("click", appendContent);
+btnPrepend.addEventListener("click", prependContent);
+
 fakeTextGeneratorSettings.paragraphMinLength = 10;
 fakeTextGeneratorSettings.paragraphMaxLength = 15;
 fakeTextGeneratorSettings.sentenceMinLength = 8;
 fakeTextGeneratorSettings.sentenceMaxLength = 15;
-refreshContent();
+regenerateAllContent();
