dd6c56f135445676dd11b40dd8195fe7ca2f276d
MatthewT95 <matt.taormina@outlook.com>
2024-07-15 10:36:20 -0400

    moved content generation outside of script.js

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-let unitCount = 3;
+let unitCount = 5;
 let unitType = "p";
 let plainText = false;
 
 function refreshContent() {
   let outputElement = document.getElementById("content");
 
-  outputElement.innerHTML = "";
-
-  if (plainText) {
-    if (unitType == "p") {
-      for (let i = 0; i < unitCount; i++) {
-        outputElement.innerHTML += "<p>" + randomFakeParagraph() + "</p>";
-      }
-    } else if (unitType == "s") {
-      let text = "";
-      for (let i = 0; i < unitCount; i++) {
-        text += randomFakeSentence();
-      }
-      outputElement.innerHTML = "<p>" + text + "</p>";
-    }
-  } else {
-    if (unitType == "p") {
-      for (let i = 0; i < unitCount; i++) {
-        outputElement.innerHTML +=
-          "&lt;p&gt;<br>" + randomFakeParagraph() + "<br>&lt;/p&gt;<br>";
-      }
-    } else if (unitType == "s") {
-      let text = "";
-      for (let i = 0; i < unitCount; i++) {
-        text += randomFakeSentence();
-      }
-      outputElement.innerHTML = "&lt;p&gt;<br>" + text + "<br>&lt;/p&gt;";
-    }
-  }
-
-  if (!plainText) {
-    outputElement.innerHTML = "<code>" + outputElement.innerHTML + "</code>";
+  if (unitType == "p") {
+    paragraphFill(outputElement, unitCount);
+  } else if (unitType == "s") {
+    sentenceFill(outputElement, unitCount);
   }
 }
 
