4c339b61dfb9d4e9ea80f6516e1cfb088151844e
MatthewT95 <matt.taormina@outlook.com>
2024-07-16 19:35:35 -0400

    Created tableFill function for generating tables.

--- FTG-AutoFill.js
+++ FTG-AutoFill.js
@@ -126,3 +126,45 @@ function sentenceFill(
       targetElement.innerHTML;
   }
 }
+
+function tableFill(targetElement, mode, genSettings = {}) {
+  let generatedHTML = "";
+
+  let {
+    sentenceMinLength,
+    sentenceMaxLength,
+    tableRowCount,
+    tableColumnCount,
+    tableHeadersOn,
+  } = genSettings;
+
+  generatedHTML += "<table>";
+  if (tableHeadersOn) {
+    generatedHTML += "<tr>";
+    for (let j = 0; j < tableColumnCount; j++) {
+      generatedHTML += "<th>" + generateFakeSentence(1, 3, false) + "</th>";
+    }
+    generatedHTML += "</tr>";
+  }
+  // Generate table rows and cells in html
+  for (let i = 0; i < tableRowCount; i++) {
+    generatedHTML += "<tr>";
+    for (let j = 0; j < tableColumnCount; j++) {
+      generatedHTML +=
+        "<td>" +
+        generateFakeSentence(sentenceMinLength, sentenceMaxLength) +
+        "</td>";
+    }
+    generatedHTML += "</tr>";
+  }
+  generatedHTML += "</table>";
+
+  // output generated html embedded in root tags
+  if (mode == "u") {
+    targetElement.innerHTML = generatedHTML;
+  } else if (mode == "a") {
+    targetElement.innerHTML += generatedHTML;
+  } else if (mode == "p") {
+    targetElement.innerHTML = generatedHTML + targetElement.innerHTML;
+  }
+}
